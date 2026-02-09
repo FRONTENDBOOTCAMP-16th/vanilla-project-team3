83c9dbae6d744d281e0116f441e67e8c466f0060
myj9713 <myj9713@gmail.com>
2026-02-09 17:38:07 +0900

    [docs] 폰트 폴더 작성, REDME 작성

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,8 +10,8 @@ export default [
       sourceType: 'module',
       globals: {
         ...globals.browser,
-        ...globals.node
-      }
-    }
-  }
+        ...globals.node,
+      },
+    },
+  },
 ]
--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,5 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-  }
+  },
 })
