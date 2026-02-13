0269f8c190189cbd1fa9ab17acc646c1f8e80956
songha-5 <shn0518@gmail.com>
2026-02-13 21:57:59 +0900

    [feat] getData추가

--- src/js/common/_json.js
+++ src/js/common/_json.js
@@ -67,35 +67,4 @@ async function getData(key, value) {
   }
 }
 
-async function postData() {
-  try {
-    // 헤더 타입
-    const headers = {
-      'Content-type': 'application/json',
-    }
-    // 해당 데이터 URL읽어오기
-    const response = await fetch(
-      `https://69898725c04d974bc69f8907.mockapi.io/todayPhrase/todaysPhrase`,
-      // 아이디 비밀번호 저장은 비공개값으로 저장해야하는데
-      // 어디에 저장하고 어떻게 비공개로 돌리나요?
-
-      {
-        // POST방식, 캐시기본값(캐시가 오래되면 새로불러옴)
-        method: 'POST',
-        headers,
-        cache: 'default',
-      },
-    )
-
-    // 에러코드 200이 아니면 에러로 간주
-    if (!response.ok) {
-      console.log('데이터 전달 실패')
-      throw new Error('실패')
-    }
-  } catch (error) {
-    // 에러페이지 추후 추가
-    console.log(error)
-  }
-}
 getData('mood').then((res) => console.log(res))
-postData().then((res) => console.log(res)) // 에러남 확인 중
