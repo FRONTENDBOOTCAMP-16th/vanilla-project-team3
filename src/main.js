456c9c5a073438b77090c7ba5e32f1f3e14f6785
songha-5 <shn0518@gmail.com>
2026-02-17 10:05:18 +0900

    [feat] api 경로 변경

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import './css/style.css'
 
 // JS 호출
-import './js/common/_json.js'
+import './../api/api.js'
 import './js/components/_checked.js'
 import './js/components/_popup.js'
 import './js/components/_share.js'
