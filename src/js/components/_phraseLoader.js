a8c063a3eb29b946766bde8789892d87ec97da4d
sasasak <geun02young@naver.com>
2026-02-17 23:33:00 +0900

    [feat] 데이터 필터링, 랜덤값 뽑기, 메인 책 + 추천 책 3권 렌더링하기 (중간 백업)

--- src/js/components/_phraseLoader.js
+++ src/js/components/_phraseLoader.js
@@ -1,40 +1,84 @@
-// 데이터 받아오기
-function loadJsonData() {
-  fetch('https://69898725c04d974bc69f8907.mockapi.io/todayPhrase/todaysPhrase')
-    .then((response) => response.json())
-    .then((data) => {
-      const jsonDataList = data.map((list) => {
-        return {
-          bookTitle: list.bookTitle,
-          phrase: list.phrase,
-          author: list.author,
-          bookCover: list.bookCover,
-        }
-      })
-      const randomIndex = Math.floor(Math.random() * jsonDataList.length)
-      const selectedData = jsonDataList[randomIndex]
-      console.log('선택된 데이터 : ', selectedData)
+// 사용자가 선택한 값 받아오기(valueName으로 들어올 수 있는 값 : checkbox-mood, checkbox-weather)
+function getSelectedValues(valueName) {
+  const checkedInputs = document.querySelectorAll(
+    `input[name="${valueName}"]:checked`,
+  )
+  return Array.from(checkedInputs).map((input) => input.dataset.value)
+}
 
-      displayPhraseResult(selectedData)
-    })
-    .catch((error) => console.log('데이터를 가져오지 못했습니다: ', error))
+// 사용자가 선택한 데이터 필터링하기
+function filterData(allData, selectedMoods, selectedWeathers) {
+  const filteredData = allData.filter((item) => {
+    // 기분 필터: 선택된 기분이 없으면 기분 전체 포함, 있으면 해당 기분만 포함
+    const matchMood =
+      selectedMoods.length === 0 || selectedMoods.includes(item.mood)
+    // 날씨 필터: 선택된 날씨가 없으면 날씨 전체 포함, 있으면 해당 날씨만 포함
+    const matchWeather =
+      selectedWeathers.length === 0 || selectedWeathers.includes(item.weather)
+    // 최종 결과: 기분과 날씨 조건을 모두 만족해야 필터링됨
+    return matchMood && matchWeather
+  })
+  // 가져온 전체 데이터에서 사용자가 선택한 키값이 같은 경우를 필터링
+  return filteredData
+}
+
+// 필터링 된 데이터에서 랜덤으로 뽑기
+function getRandomData(filteredData, count) {
+  const result = []
+  // 원본을 훼손하지 않을 복사본 데이터
+  const copyData = [...filteredData]
+
+  for (let i = 0; i < count; i++) {
+    if (copyData.length === 0) break
+
+    const randomIndex = Math.floor(Math.random() * copyData.length)
+
+    // 랜덤으로 고른 인덱스 번호의 데이터를 복사본에서 잘라내어 result에 넣기
+    // 복사본 배열에서 아예 빼버리고 다시 랜덤으로 돌려서 뽑아내는것이므로 중복이 발생하지않음
+    const seletedRandomData = copyData.splice(randomIndex, 1)[0]
+    result.push(seletedRandomData)
+  }
+  return result
 }
 
 // 화면에 글자와 이미지 변경하기
 function displayPhraseResult(selectedJsonData) {
+  if (selectedJsonData.length === 0) return
+
+  const mainRecommendData = selectedJsonData[0]
   const bookTitle = document.querySelector('.book-title')
   const bookAuthor = document.querySelector('.author')
   const recommendPhrase = document.querySelector('.phrase')
   const resultDisplay = document.querySelector('.result-display')
 
-  bookTitle.textContent = selectedJsonData.bookTitle
-  bookAuthor.textContent = selectedJsonData.author
-  recommendPhrase.textContent = selectedJsonData.phrase
+  bookTitle.textContent = mainRecommendData.bookTitle
+  bookAuthor.textContent = mainRecommendData.author
+  recommendPhrase.textContent = mainRecommendData.phrase
 
   resultDisplay.style.setProperty(
     '--bg-image',
-    `url(${selectedJsonData.bookCover})`,
+    `url(${mainRecommendData.bookCover})`,
   )
+
+  const bookCoverImages = document.querySelectorAll('.book-cover-img')
+  const subBooks = selectedJsonData.slice(1)
+  subBooks.forEach((data, index) => {
+    if (bookCoverImages[index]) {
+      bookCoverImages[index].src = data.bookCover
+      bookCoverImages[index].alt = data.bookTitle
+    }
+  })
 }
 
-loadJsonData()
+const testCheckButton = document.querySelector('.user-test-check')
+
+testCheckButton.addEventListener('click', async () => {
+  const allData = await getData()
+  const moods = getSelectedValues('checkbox-mood')
+  const weathers = getSelectedValues('checkbox-weather')
+
+  const filteredData = filterData(allData, moods, weathers)
+  const selectedData = getRandomData(filteredData, 4)
+
+  displayPhraseResult(selectedData)
+})
