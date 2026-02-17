5c5d0a30c6aa3a72f21b31e909c862e005b2c56c
songha-5 <shn0518@gmail.com>
2026-02-17 13:36:58 +0900

    [feat]멀티페이지 설정

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   server: {
     port: 3000,
     open: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        // 키(key): 값(파일 경로) 형태로 모든 페이지를 등록
+        main: resolve(__dirname, 'index.html'), // 홈 페이지
+        about: resolve(__dirname, 'about.html'), // 소개 페이지
+        login: resolve(__dirname, 'login/index.html'), // 로그인 페이지 (폴더 구조)
+      },
+    },
   },
 })
