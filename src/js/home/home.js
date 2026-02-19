45514de648f5e1fe202915e5d048dd1486f6258d
songha-5 <shn0518@gmail.com>
2026-02-19 11:13:52 +0900

    [refactor]코드 주석 추가

--- src/js/home/home.js
+++ src/js/home/home.js
@@ -56,8 +56,11 @@ function bindEvents() {
 function handleResetAllInput() {
   // 브라우저 뒤로가기로 왔을 때
   // 페이지 초기화 과정에서 로컬 스토리지의 키 제거
+  
+  // 해당 코드를 지워도 브라우저 뒤로가기/앞으로 가기가 잘 작동함 =========================
   // removeStorage(IS_CHECKED_KEY)
   // removeStorage(IMOJI)
+  // 해당 코드를 지워도 브라우저 뒤로가기/앞으로 가기가 잘 작동함 =========================
 
   // 감정/날씨 체크된것 다 지우기
   doubleCheckedGroups.forEach((group) => {
