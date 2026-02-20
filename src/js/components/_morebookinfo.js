80d819bdd91aab20619040eb304504b85332e228
myj9713 <myj9713@gmail.com>
2026-02-20 11:45:00 +0900

    [refactor] js 코드를 함수형으로 수정

--- src/js/components/_morebookinfo.js
+++ src/js/components/_morebookinfo.js
@@ -1,13 +1,20 @@
-const recommendLists = document.querySelector('.recommend-lists')
-
 // 링크를 연결하는 함수는 phraseLoader.js에 연결해 둠
 
-// 클릭 로그 확인
-if (recommendLists) {
-  recommendLists.addEventListener('click', (event) => {
-    const target = event.target.closest('.more-book-info')
-    if (target) {
-      console.log('이동할 주소:', target.href)
-    }
-  })
+// 클릭했을 때 실행될 핸들러 함수
+function handleLinkClick(event) {
+  const target = event.target.closest('.more-book-info')
+  if (target) {
+    console.log('이동할 주소:', target.href)
+  }
 }
+
+// 이벤트 연결하는 함수
+export function initRecommendEvents(element) {
+  if (element) {
+    element.addEventListener('click', handleLinkClick)
+  }
+}
+
+// 실행 시
+const recommendLists = document.querySelector('.recommend-lists')
+initRecommendEvents(recommendLists)
