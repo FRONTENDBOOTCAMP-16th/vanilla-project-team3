cb228ebe0dbdbe1f32f81d598f3097e99ad3c334
songha-5 <shn0518@gmail.com>
2026-02-20 15:45:21 +0900

    [feat] 로그인 기능 추가

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,43 +1,13 @@
-import { getData, getUser } from '../../../api/api'
+import { getUser } from '../../../api/api'
 
 const form = document.querySelector('.autu-box-container')
 if (!form) throw new Error('문서에서 form을 찾을 수 없습니다.')
 const id = form.querySelector('.id-box')
-const idNoti = form.querySelector('.id-blank-warning')
 const password = form.querySelector('.pw-box')
-// const passwordNoti = form.querySelector('.pw-blank-warning')
+const noti = form.querySelector('.noti-blank-warning')
 const login = form.querySelector('.submit-button')
 
-// getData('mood')
-// getUser('id')
-
-getData('mood', 'happy').then((res) => console.log(res))
-
-// TODO
-// 로그인 버튼을 눌렀을때 GET으로 조회해서 아이디와 비밀번호 맞는지 비교
-// 비교? 어떻게? if를 쓰자
-// 각각의 input에게 유효성 걸어야함
-// 로그인 눌렀을때 get이랑 값 비교하여
-// 1.아이디가 동일하지 않음 알람 선행
-// > 아이디가 동일한데 비밀번호가 틀린경우
-// 2. 비밀번호가 동일하지 않음 알람 후행
-// 확인 디스에이블 > 에이블
-
 // 페이지 뒤로가기 / 앞으로가기 했을때 데이터 싹 다 날아가야함
-
-// 1.
-// 로그인 버튼 눌릴때 a 버튼 막기
-// disabled일때 기능 막기
-// disabled 태그 추가
-
-// 2.
-// 로그인 눌렀을때 GET으로 데이터 조회
-// 데이터 조회한것으로 input 아이디/ input password 비교
-
-// 3.
-// 비밀번호/아이디 들중하나 맞지 않는 다면 e.preventEvent()
-// 아이디가 틀린경우 > 아이디 노티
-// 아이디가 맞고 비밀번호가 틀린 경우 > 비빌번호 노티
 init()
 
 function init() {
@@ -55,24 +25,24 @@ function handleFormClick(e) {
   const target = e.target
 
   if (target === login) {
+    // 아이디와 비밀번호 입력 안되었을 시 로그인안됨
     if (!id.value || !password.value) {
       e.preventDefault()
     } else {
-      getUser('email', id.value) === undefined ? (idNoti.hidden = false) : true
-      getUser('email', id.value).then((res) => console.log(res))
+      // 아이디와 비밀번호 입력 체크
+      checkeEmailPassword()
+
       e.preventDefault()
     }
-
     return
   }
 
-  if (target !== id && target !== password) return
+  if (target !== id) return
 
   if (target === id) {
+    // TODO
+    // 영문,숫자,소문자만 입력되도록 수정하기
     console.log('아이디')
-  }
-  if (target === password) {
-    console.log('비번')
   }
 }
 
@@ -85,4 +55,22 @@ function handleFormChange() {
   }
 }
 
-// getUser('email', 'anc1111@naver.com').then((res) => console.log(res))
+// 이메일과 비밀번호 확인
+async function checkeEmailPassword() {
+  const resultID = await getUser('email', id.value)
+  const isPassword = resultID.password === password.value
+
+  // 아이디와 비밀번호와 동일하지않으면 안내문구
+  resultID && isPassword ? (noti.hidden =  true) : (noti.hidden = false)
+
+  // 아이디와 비밀번호가 맞다면 로그인 성공
+  isLogin(resultID, isPassword)
+}
+
+// 로그인 후 메인페이지로 이동
+async function isLogin(resultID, resultPassword) {
+  if (resultID && resultPassword) {
+    alert('로그인이 성공하였습니다.')
+    window.location.href = 'http://localhost:3000/index.html'
+  }
+}
