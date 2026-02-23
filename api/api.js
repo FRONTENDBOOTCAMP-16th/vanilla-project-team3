e686fb86c6631659947865bd3980581585faa96c
songha-5 <shn0518@gmail.com>
2026-02-23 18:32:41 +0900

    [feat] 찜 리스트 호출을 위한 임시 post 기능 삭제

--- api/api.js
+++ api/api.js
@@ -111,32 +111,3 @@ export async function getUser(key, value) {
     return null
   }
 }
-
-// 유저 데이터를 추가하는 함수
-export async function postData(url, dataObj) {
-  try {
-    // 헤더 타입
-    const headers = {
-      'Content-type': 'application/json',
-    }
-
-    // 데이터를 JSON 문자열로 변환
-    const payload = JSON.stringify(dataObj)
-
-    // 해당 데이터 URL읽어오기
-    const response = await fetch(url, {
-      method: 'POST',
-      headers,
-      body: payload,
-    })
-
-    // 에러코드 200이 아니면 에러로 간주
-    if (!response.ok) {
-      console.log('데이터 전달 실패')
-      throw new Error('실패')
-    }
-  } catch (error) {
-    // 에러 콘솔
-    console.log(error)
-  }
-}
